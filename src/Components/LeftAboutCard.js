ec4d9d512f4392a3c7f44945aaa104b7ac968e84
Rajan kumar <krcpr007@gmail.com>
2022-01-12 22:04:14 +0530

    left side i.e profile info done

---
+++ src/Components/LeftAboutCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function LeftAboutCard() {
+    return (
+        <div className='shadow w-2/3 mt-2 rounded ml-10'>
+            <div className='ml-12 mt-2 '>
+               <a href="/"><img src="https://avatars.githubusercontent.com/u/80947662?v=4" alt="" className='w-2/3 content-center rounded-full' /></a>
+            </div>
+            <h1 className='mt-1 font-medium text-xl text-center px-5'>Rajan Kumar</h1>
+            <p className='text-sm text-gray-500 px-5 text-center'>
+                Full stack MERN Developer|| CSE Student at NITP || 
+                React Fronted Developer
+            </p>
+            <hr  className='my-2'/>
+            <div className='px-5 text-sm'>
+             <p >Number of followers <a href="/"> <span className='text-blue-500'> 3.5k</span> </a></p>
+             <p>Profile views this month <a href="/"><span className='text-blue-500'>1.5k</span></a> </p>
+             <p >Post views this month <a  href="/"><span className='text-blue-500'>33.5k</span></a> </p>
+            </div>
+        </div>
+    )
+}
+
+export default LeftAboutCard; 
