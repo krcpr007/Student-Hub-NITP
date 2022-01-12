9b78edd7530745bea06f97d96615f201e0689608
Rajan kumar <krcpr007@gmail.com>
2022-01-12 22:07:41 +0530

    Merging every component but navbar not modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import PostCard from "./Components/PostCard";
+import NewPost from "./Components/NewPost";
+import LeftAboutCard from "./Components/LeftAboutCard";
+import GroupsClub from "./Components/GroupsClub";
+import PeersNews from "./Components/PeersNews";
+import Opportunities from "./Components/Opportunities";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navbar />
+      <div className="flex">
+        <div>
+          <div className="hidden md:inline">
+            <LeftAboutCard />
+            <GroupsClub />
+          </div>
+        </div>
+        <div className="">
+          <NewPost />
+          <PostCard />
+        </div>
+        <div className="hidden md:inline">
+          <PeersNews />
+          <Opportunities />
+        </div>
+      </div>
     </div>
   );
 }
