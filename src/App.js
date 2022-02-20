7e2c9ecdaa914ecc1705477d06b010ffc921b7fa
Rajan kumar <krcpr007@gmail.com>
2022-02-20 17:31:14 +0530

    routes created

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import PostCard from "./Components/PostCard";
-import NewPost from "./Components/NewPost";
-import LeftAboutCard from "./Components/LeftAboutCard";
-import GroupsClub from "./Components/GroupsClub";
-import PeersNews from "./Components/PeersNews";
-import Opportunities from "./Components/Opportunities";
+import Navbar from "./Components/NavBar/Navbar";
+import Error from "./Components/Error/404";
+import Signup from './Components/Auth/Signup'
+import Login from './Components/Auth/Login'
+import Footer from './Components/Footer/Footer'
+import Feed from "./Components/Feed/Feed";
 function App() {
   return (
-    <div className="">
-      <Navbar />
-      <div className="flex">
-        <div>
-          <div className="hidden md:inline">
-            <LeftAboutCard />
-            <GroupsClub />
-          </div>
-        </div>
-        <div className="">
-          <NewPost />
-          <PostCard />
-        </div>
-        <div className="hidden md:inline">
-          <PeersNews />
-          <Opportunities />
-        </div>
-      </div>
-    </div>
+    <>
+     <BrowserRouter>
+          <Navbar />
+          <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Feed />} />
+            <Route path="/*" element={<Error />} />
+          </Routes>
+          {/* <ToastContainer theme="dark" /> */}
+          <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
