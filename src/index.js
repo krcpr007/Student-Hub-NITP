df0e719732181d37410f68e190f898e6016240b9
Rajan kumar <krcpr007@gmail.com>
2022-02-23 21:22:10 +0530

    context moved to app.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {ContextProvider} from './Components/context/ContextProvider'
 ReactDOM.render(
   <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </React.StrictMode>
-  ,
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
