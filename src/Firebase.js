6475dccbf151ba1fa1f65e55b703596d87e707bd
Rajan kumar <krcpr007@gmail.com>
2022-03-01 23:31:25 +0530

    db export

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,3 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const storage = getStorage(app);
+const db = getFirestore(app); 
+export {db , storage}
