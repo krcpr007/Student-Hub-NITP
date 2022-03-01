dde18fcd0109057092f042b89711535e11e38039
Rajan kumar <krcpr007@gmail.com>
2022-03-01 11:51:17 +0530

    profile pic and name integrated

--- src/Components/Feed/LeftAboutCard.js
+++ src/Components/Feed/LeftAboutCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import { getAuth } from "firebase/auth";
 function LeftAboutCard() {
+    const auth = getAuth(); 
     return (
         <div className='shadow w-2/3 mt-2 rounded ml-10'>
             <div className='ml-12 mt-2 '>
-               <a href="/"><img src="https://avatars.githubusercontent.com/u/80947662?v=4" alt="" className='w-2/3 content-center rounded-full' /></a>
+               <a href="/"><img src={`${auth.currentUser?auth.currentUser.photoURL:'https://avatars.githubusercontent.com/u/80947662?v=4'}`} alt="" className='w-2/3 content-center rounded-full' /></a>
             </div>
-            <h1 className='mt-1 font-medium text-xl text-center px-5'>Rajan Kumar</h1>
+            <h1 className='mt-1 font-medium text-xl text-center px-5'>{auth.currentUser?`${auth.currentUser.displayName}`:'</Rajan kumar>'}</h1>
             <p className='text-sm text-gray-500 px-5 text-center'>
                 Full stack MERN Developer|| CSE Student at NITP || 
                 React Fronted Developer
