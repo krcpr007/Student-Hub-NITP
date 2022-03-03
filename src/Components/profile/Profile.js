8296e308178b9ff6560ea10d90f68e996cde3dfe
Rajan kumar <krcpr007@gmail.com>
2022-03-03 10:51:33 +0530

    modal style

--- src/Components/profile/Profile.js
+++ src/Components/profile/Profile.js
@@ -91,7 +91,7 @@ function Profile() {
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-1/4 my-6 mx-auto max-w-3xl ">
+            <div className="relative md:w-1/4 my-6 mx-auto max-w-3xl ">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-slate-600 outline-none focus:outline-none">
                 {/*header*/}
@@ -119,21 +119,21 @@ function Profile() {
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                   <button
-                    className="background-transparent font-bold px-5 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className=" bg-slate-800 text-white active:bg-slate-600 font-bold uppercase text-sm p-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    <AiFillCloseCircle className="text-3xl"/>
+                    <AiFillCloseCircle className=""/>
                   </button>
                   <button
-                    className="background-transparent font-bold px-5 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="bg-rose-800 text-white active:bg-slate-600 font-bold uppercase text-sm px-3 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={deleteProfileImg}
                   >
-                    <MdDelete className="text-3xl"/>
+                    <MdDelete className=""/>
                   </button>
                   <button
-                      className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      className="bg-slate-800 text-white active:bg-slate-600 font-bold uppercase text-sm px-3 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                       type="button"
 
                     >
