b16d06b735fa4f3c349e874d0f92cb2ab094bf28
Rajan kumar <krcpr007@gmail.com>
2022-03-06 18:03:50 +0530

    conversation comp for display msgs and file

---
+++ src/Components/Messages/ConversationsText.js
@@ -0,0 +1,32 @@
+import React, {useRef , useEffect} from 'react'
+import { useParams } from 'react-router-dom';
+import { auth } from '../../Firebase';
+import Moment from 'react-moment';
+function ConversationsText({ msg }) {
+    const params = useParams(); 
+    const {uid} = params; 
+    console.warn(uid)
+    // when user enter the msg it scorll the screen to last msg
+    const scrollRef =useRef();
+    useEffect(()=>{
+         scrollRef.current?.scrollIntoView({behavior:"smooth"});
+    },[msg])
+    return (
+        <div className='' ref={scrollRef}>
+            <div className="">
+                <div className='px-0 '>
+                    <p className={`m-1 ${msg.to===auth.currentUser.uid?'text-left text-blue-800':'text-right'}`}>{msg.msg}
+                      {msg.media ?(<img src={msg.media} />):null}
+                        <br />
+                        <small className='text-xs'>
+                            {<Moment fromNow>
+                                {msg.createdAt.toDate()}</Moment>}
+                        </small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ConversationsText
