2c72bc2d72bcb7a8b9d7c53a2cf2e930989da4ae
Rajan kumar <krcpr007@gmail.com>
2022-03-06 18:24:46 +0530

    error styling

--- src/Components/Error/404.js
+++ src/Components/Error/404.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import bgimg from './login-hero.svg'
 function Error() {
   return (
-    <div>Error</div>
+    <>
+    <div className="p-0 flex">
+       <img src={bgimg} className="w-1/4" alt="" />
+      <div>
+        <h1>404</h1>
+        <span>Page doesn't exist </span>
+      </div>
+    </div>
+    </>
   )
 }
 
