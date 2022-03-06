a86c70ba6bc190ef55ae71d2d5901c21c5d7f73d
Rajan kumar <krcpr007@gmail.com>
2022-03-06 18:06:08 +0530

    msg sending and function created and uploading file which not working now

--- src/Components/Messages/Chat.js
+++ src/Components/Messages/Chat.js
@@ -1,42 +1,95 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import SenderProfile from './SenderProfile'
 import { useParams } from "react-router-dom";
-import {BsUpload} from 'react-icons/bs'
-import { doc,  getDoc } from "firebase/firestore";
-import { db} from '../../Firebase';
+import { BsUpload } from 'react-icons/bs'
+import {
+  doc,
+  getDoc, addDoc,
+  collection, Timestamp, orderBy, query
+  , onSnapshot,
+} from "firebase/firestore";
+import {
+  ref, getDownloadURL, uploadBytes,
+  deleteObject
+} from 'firebase/storage'
+import { db, auth, storage } from '../../Firebase';
+import ConversationsText from './ConversationsText';
 function Chat() {
+  const user1 = auth.currentUser.uid; // getting current user  
   const params = useParams();
   const { uid } = params;
-  const userid = uid.split(" ").join("") //removing spaces form uid it was most important for geting user profile info
+  const user2 = uid // uid we are getting from params
+  // const userid = uid.split(" ").join("") //removing spaces form uid it was most important for geting user profile info
   const [user, setUser] = useState({});
+  const [conversation, setConversation] = useState([])
+  const [msg, setMsg] = useState('');
+  const [media, setMedia] = useState('');
   useEffect(() => {
     // userInformation();
-    const docRef = doc(db, "users", userid);
-      getDoc(docRef).then((docSnap)=>{
-        if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-      setUser(docSnap.data());
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-    }
-      
-      }).catch((e)=>console.log(e))
-
+    const docRef = doc(db, "users", uid);
+    getDoc(docRef).then((docSnap) => {
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        setUser(docSnap.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }).catch((e) => console.log(e))
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}` // creating msg unique id
+    const msgsRef = collection(db, 'messages', id, 'chat');
+    const q = query(msgsRef, orderBy('createdAt', 'asc')) // for geting msgs in descending order
+    onSnapshot(q, querySnapshot => {
+      let convo = [];
+      querySnapshot.forEach((doc) => {
+        convo.push(doc.data());
+      })
+      setConversation(convo)
+    })
   }, [])
+  // console.warn(conversation)
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}` // creating msg unique id
+    // if user is selected the url 
+    let url;
+    if (media) {
+      const mediaRef = ref(storage, `ChatMedia/${new Date().getTime()} - ${media.name}`);
+      const snap = await uploadBytes(mediaRef, media);
+      const MediaUrl = getDownloadURL(ref(storage, snap.ref.fullPath))
+      url = MediaUrl;
+    }
+    await addDoc(collection(db, 'messages', id, 'chat'), {
+      msg,
+      from: user1,
+      to: user2,
+      createdAt: Timestamp.fromDate(new Date()),
+      media: url || ''
+    });
+    setMsg('');
+  }
   return (
     <>
-        <div>
-            <SenderProfile />
-            <div className='p-5'>
-                
-            </div>
-            <div className='relative bottom-0 flex'>
-                <BsUpload className=''/>
-                <input type="text" className='bg-gray-500 px-2 py-2 w-3/4' />
-                <button className='bg-yellow-300 px-2 py-2 rounded-r'>Send</button>
-            </div>
+      <div>
+        <SenderProfile sender={user} />
+        <div className='bg-slate-700'>
+          <div>
+            {conversation.length && conversation.map((msg, i)=> <ConversationsText key={i} msg={msg} />)}
+          </div>
         </div>
+
+        <div className='relative bottom-0 flex'>
+          <form className='flex'>
+            <label htmlFor="file">
+              <BsUpload className='text-2xl mx-3 my-2' />
+            </label>
+            <input type="file" accept="image/*" style={{ display: "none" }} onChange={e => setMedia(e.target.files[0])} name='file' id='file' />
+            <input type="text" value={msg} onChange={e => setMsg(e.target.value)} className='bg-gray-500 px-2 py-2 ' />
+            <button type="submit" onClick={handleSubmit} className='bg-yellow-300 px-2 py-2 rounded-r'>Send</button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
