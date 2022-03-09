c7310c100abdf0e108d97aa807b11589aa7104ef
Rajan kumar <krcpr007@gmail.com>
2022-03-09 17:28:42 +0530

    Contact modal created and integrated

--- src/Components/profile/Profile.js
+++ src/Components/profile/Profile.js
@@ -5,7 +5,7 @@ import {BsFillCameraFill} from 'react-icons/bs';
 import {FaGithub} from 'react-icons/fa'
 import  {ImLinkedin} from 'react-icons/im'
 import {RiInstagramFill} from 'react-icons/ri'
-import {MdDelete} from 'react-icons/md'
+import {MdDelete, MdEmail ,MdContactPhone ,MdHome} from 'react-icons/md'
 import { Link } from "react-router-dom";
 import avatar from '../assets/img_avatar.png'
 import Loader from "../Loader/Loader";
@@ -17,7 +17,7 @@ function Profile() {
   const {darkMode ,profileData ,userInformation}= useContext(ContextProvider); 
   const [profileImg ,setProfileImg]=useState();
   const [showModal, setShowModal] = React.useState(false);
-  const [showContactModal, setShowContactModal] = useState(true);
+  const [showContactModal, setShowContactModal] = useState(false);
   const [loader, setLoader] = useState(false);
   useEffect(() => {
     userInformation();
@@ -74,7 +74,6 @@ function Profile() {
   }
   return (
     <>
-     {/* https://www.w3schools.com/howto/img_avatar.png */}
     <div>
       <div className="w-full md:w-3/4 md:px-24 md:p-2 ">
         <div className={`shadow-2xl md:rounded-t-lg ${darkMode?'bg-slate-900 text-white' :null}`}>
@@ -111,7 +110,7 @@ function Profile() {
             <h1 className="text-3xl font-medium">{auth.currentUser.displayName?`${auth.currentUser.displayName}`:`${profileData.name}`}</h1>
 
             <span className="text-sm">{profileData && profileData.headline}</span> <br />
-            <span className="text-xs">Patna, Bihar,India</span> <Link className="text-sm text-blue-600" to="/contactInfo">Contact info</Link>
+            <span className="text-xs">Patna, Bihar,India</span> <span className="text-sm text-blue-600 cursor-pointer" onClick={e=>setShowContactModal(true)}  >Contact info</span>
           </div>
         </div>
       </div>
@@ -167,7 +166,6 @@ function Profile() {
 
                     >
                       <label htmlFor="profileImg">
-
                         <BsFillCameraFill className="text-lg" />
                       </label>
                       <input type="file" accept="image/*" style={{ display: "none" }} id="profileImg" onChange={e => setProfileImg(e.target.files[0])} />
@@ -176,9 +174,61 @@ function Profile() {
               </div>
             </div>
           </div>
-          {/* <div className="opacity-25 fixed inset-0 z-40 bg-black"></div> */}
         </>
       ) : null}
+      {showContactModal?(<>
+        <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative md:w-1/4 my-6 mx-auto max-w-3xl ">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-200 outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-xl font-medium my-4">
+                    Contact Info
+                  </h3>
+                  <img src="nitlogo.png" className="mx-5" alt="" />
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-4 flex-auto">
+                <div className="flex my-2">
+                <MdEmail className="text-2xl text-yellow-500"/>
+                <a href={`mailto:${auth.currentUser.email}`} className="mx-2">{auth.currentUser?.email}</a>
+                </div>
+                <div className="flex my-2">
+                  <MdContactPhone className="text-2xl text-yellow-500"/>
+                   <a title="Tap to call" href={`tel:${profileData.contactInfo?.phoneNo}`} className="mx-2">{profileData.contactInfo?.phoneNo}</a>
+                </div>
+                <div className="flex my-2">
+                  <MdHome className="text-2xl text-yellow-500" />
+                  <p className="mx-2">{profileData.contactInfo?.home}</p>
+                  </div>
+                
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className=" bg-slate-800 text-white active:bg-slate-600 font-bold uppercase text-sm p-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowContactModal(false)}
+                  >
+                    <AiFillCloseCircle className=""/>
+                  </button>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+      </>):null}
     </div>
  </>
   );
