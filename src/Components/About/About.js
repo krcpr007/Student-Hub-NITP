52b0a777c22238875aba1cfdf0b8d2c6f40245d2
Rajan kumar <krcpr007@gmail.com>
2022-03-15 11:49:30 +0530

    skelton created

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <div>
+        <div>
+          <img src="nithindi.png" className='bg-slate-900 h-10' alt="Nit-hindi" />
+        </div>
+        <div className='mt-5 px-2 sm:px-5'>
+          <h1 className='text-2xl'>Vision</h1>
+          <hr />
+          <p className='opacity-75'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique id debitis unde, quaerat quod reprehenderit obcaecati veniam, temporibus nobis ut labore saepe earum pariatur exercitationem nostrum. Ipsa aliquam libero at!</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
