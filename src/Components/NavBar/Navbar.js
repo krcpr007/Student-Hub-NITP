39e51f73153e36a478ad8f9d2dd9ac1636e1aabf
Rajan kumar <krcpr007@gmail.com>
2022-03-15 11:50:03 +0530

    search removed if user is not loged In

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -3,18 +3,18 @@ import { Link, useNavigate } from 'react-router-dom'
 import { ImHome } from 'react-icons/im'
 import { FaUserPlus } from 'react-icons/fa'
 import { IoMdLogIn } from 'react-icons/io'
-import { AiFillCaretDown ,AiFillMessage } from 'react-icons/ai'
+import { AiFillCaretDown, AiFillMessage } from 'react-icons/ai'
 import { BiNetworkChart } from 'react-icons/bi'
 import { signOut } from 'firebase/auth';
 import avatar from '../assets/img_avatar.png'
-import {auth} from '../../Firebase'
+import { auth } from '../../Firebase'
 import ContextProvider from "../context/ContextProvider";
 import Darkmode from '../DarkMode/Darkmode'
 function Navbar() {
-  const navigate = useNavigate(); 
-  const { darkMode , profileData , userInformation } = useContext(ContextProvider);
+  const navigate = useNavigate();
+  const { darkMode, profileData, userInformation } = useContext(ContextProvider);
   const [showDropDown, setShowDropDown] = useState(false);
-  const [search, setSearch] =useState('');
+  const [search, setSearch] = useState('');
   const handleLogout = () => {
     if (auth.currentUser) {
       localStorage.removeItem('st-hub')
@@ -28,12 +28,12 @@ function Navbar() {
     }
     setShowDropDown(false)
   }
-  const OnSearch=(e)=>{
+  const OnSearch = (e) => {
     e.preventDefault()
-    navigate(`/search?name=${search}`); 
+    navigate(`/search?name=${search}`);
     setSearch('');
   }
-  const showAndHideDropDown =()=>{
+  const showAndHideDropDown = () => {
     showDropDown ? setShowDropDown(false) : setShowDropDown(true)
   }
   return (
@@ -46,20 +46,22 @@ function Navbar() {
             </Link>
           </h1>
         </div>
-        <Darkmode/>
+        <Darkmode />
         <input className="menu-btn hidden" type="checkbox" id="menu-btn" />
         <label className="menu-icon block cursor-pointer md:hidden px-2 py-4 relative select-none" htmlFor="menu-btn">
           <span className="navicon bg-grey-darkest flex items-center relative"></span>
         </label>
         <ul className='menu'>
-         <form>
-          <input onChange={e=>setSearch(e.target.value)} className="px-7 py-1 border border-yellow-300 md:w-auto text-yellow-400 shadow-2xl rounded m-2 bg-transparent  active:border-yellow-400  focus:ring-yellow-500 focus:ring focus:border-yellow-400 placeholder:text-yellow-500 bg-slate-800 " placeholder="Search..." type="text" />
-          <button type='submit' onClick={OnSearch} className='hidden'>search</button>
-          </form>
+          {localStorage.getItem('st-hub') ? (
+            <form>
+              <input onChange={e => setSearch(e.target.value)} className="px-7 py-1 border border-yellow-300 md:w-auto text-yellow-400 shadow-2xl rounded m-2 bg-transparent  active:border-yellow-400  focus:ring-yellow-500 focus:ring focus:border-yellow-400 placeholder:text-yellow-500 bg-slate-800 " placeholder="Search..." type="text" />
+              <button type='submit' onClick={OnSearch} className='hidden'>search</button>
+            </form>
+          ) : null}
         </ul>
         <ul className="menu border-b md:border-none flex justify-end list-reset m-0 w-full md:w-auto">
 
-          {localStorage.getItem('st-hub')? (<>
+          {localStorage.getItem('st-hub') ? (<>
             <li className="border-t md:border-none">
               <Link to="/" className="block md:inline-block px-4 py-3 no-underline text-grey-darkest hover:text-grey-darker"> <ImHome size="30" color='gold' className='inline lg:block mx-2' /><span className='text-sm font-medium'>My Feed</span></Link>
             </li>
@@ -76,20 +78,20 @@ function Navbar() {
                 <img src={profileData.profileImg || avatar} alt="" className='w-7 rounded-full border-2 border-gray-400 inline lg:block' />
                 <span className='text-sm mx-2 lg:-mx-1.5 font-medium' id="menu-button" aria-expanded="true" aria-haspopup="true">Profile</span>
               </Link>
-                <AiFillCaretDown className='inline cursor-pointer -ml-3 mt-0' aria-expanded="true" aria-haspopup="true" onClick={showAndHideDropDown} />
-                {showDropDown ? (<>
-                  <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
-                    <div className="py-1" role="none">
-                      {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
-                      <Link to="/editProfile" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">Account settings</Link>
-                      <a href="/#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-1">Support</a>
-                      <Link to='/nitpatna' className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-2">License</Link>
-                      <Link to="/login" onClick={handleLogout} className="text-gray-700 block w-full text-left px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-3">Sign out</Link>
-                      <button onClick={showAndHideDropDown} className="text-gray-700 block w-full text-left px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-3">close</button>
-                    </div>
+              <AiFillCaretDown className='inline cursor-pointer -ml-3 mt-0' aria-expanded="true" aria-haspopup="true" onClick={showAndHideDropDown} />
+              {showDropDown ? (<>
+                <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
+                  <div className="py-1" role="none">
+                    {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
+                    <Link to="/editProfile" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">Account settings</Link>
+                    <a href="/#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-1">Support</a>
+                    <Link to='/nitpatna' className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-2">License</Link>
+                    <Link to="/login" onClick={handleLogout} className="text-gray-700 block w-full text-left px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-3">Sign out</Link>
+                    <button onClick={showAndHideDropDown} className="text-gray-700 block w-full text-left px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-3">close</button>
                   </div>
+                </div>
 
-                </>) : null}
+              </>) : null}
             </li>
           </>) : (
             <>
