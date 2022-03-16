70880d1795d15769024eb77e11bc162422a7e0dc
Rajan kumar <krcpr007@gmail.com>
2022-03-16 13:18:32 +0530

    search working properly now

--- src/Components/Search/ProfileCard.js
+++ src/Components/Search/ProfileCard.js
@@ -5,15 +5,16 @@ import { Link } from 'react-router-dom'
 function ProfileCard({ user }) {
     return (
         <div>
-            <div className='card rounded-lg shadow-2xl w-52'>
+            <div className='card rounded-lg border border-gray-200 hover:shadow-2xl w-72 sm:w-52'>
                 <div>
                     <img src="https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1178&q=80" alt="" className='rounded-t' />
-                    <img src={`${user.profileImg}` || Img} alt="" className='rounded-full w-1/3 relative bottom-10 left-16' />
+                    <Link to={`/user/${user.uid}`}>
+                        <img src={`${user.profileImg}` || Img} alt="" className='rounded-full w-1/3 relative bottom-10 left-16' />
+                    </Link>
                 </div>
                 <div className='text-center relative bottom-8'>
                     <h1>{user.name}</h1>
-                    <p className='opacity-75'>{user.headline?`${user.headline?.substring(0,20)}...`:null}</p>
-                    {/* <button className='bg-yellow-400 rounded'>Message</button> */}
+                    <p className='opacity-75'>{user.headline ? `${user.headline?.substring(0, 20)}...` : null}</p>
                     <Link to={`/chat/${user.uid}`} title="Message" className="text-md shadow-2xl text-yellow-400 outline-2 outline-yellow-400 hover:bg-yellow-400 border border-yellow-300 hover:text-white px-1.5 py-0.5 rounded-md">Message</Link>
                 </div>
             </div>
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,8 +1,7 @@
-import React, { useEffect ,useState , useContext } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { useLocation } from 'react-router-dom';
 import { db, auth } from '../../Firebase';
 import ProfileCard from './ProfileCard';
-
 import {
   collection,
   query,
@@ -13,12 +12,12 @@ import LeftAboutCard from '../Feed/LeftAboutCard';
 import PeersNews from '../Feed/PeersNews';
 import GroupsClub from '../Feed/GroupsClub';
 import Opportunities from '../Feed/Opportunities';
-import  ContextProvider  from '../context/ContextProvider';
+import ContextProvider from '../context/ContextProvider';
 function Search() {
-  const {darkMode} = useContext(ContextProvider)
+  const { darkMode } = useContext(ContextProvider)
   let querySearch = new URLSearchParams(useLocation().search).get('name');
   console.log(querySearch);
-  const [users, setUsers]= useState([])
+  const [users, setUsers] = useState([])
   useEffect(() => {
     const usersRef = collection(db, "users");
     // create query object
@@ -27,21 +26,20 @@ function Search() {
     const unsub = onSnapshot(q, (querySnapshot) => {
       let users = [];
       querySnapshot.forEach((doc) => { // here finding all the users 
-        users.push(doc.data()); 
+        users.push(doc.data());
       })
-     const filterData= users.filter((value)=>{ // and here find users by the search term which is querySearch
-        return value.name.toLowerCase().includes(querySearch.toLowerCase()) || value.headline.toLowerCase().includes(querySearch.toLowerCase()) || value.CurrentPosition.toLowerCase().includes(querySearch.toLowerCase()) // logic to search  
-        // search in these fields name , headline, adress , mob,
-        // || value.contactInfo.home.toLowerCase().includes(querySearch.toLowerCase())
-        // || value.contactInfo.phone.toLowerCase().includes(querySearch.toLowerCase())
+      const filterData = users.filter((value) => { // and here find users by the search term which is querySearch
+        //logic to search user the inside details of users 
+        // search via name ,headline and addresses also can be done 
+        return (value.name.toLowerCase().includes(querySearch.toLowerCase()) || value.headline.toLowerCase().includes(querySearch.toLowerCase()) || value.contactInfo.home.toLowerCase().includes(querySearch.toLowerCase()))
       })
-     setUsers(filterData);
+      setUsers(filterData);
     });
     return () => unsub();
   }, [querySearch])
   return (
     <div>
-      <div className={`${darkMode?'bg-slate-900 text-white':null}`}>
+      <div className={`${darkMode ? 'bg-slate-900 text-white' : null}`}>
         <div className="flex">
           <div>
             <div className="hidden md:inline">
@@ -49,14 +47,14 @@ function Search() {
               <GroupsClub />
             </div>
           </div>
-          <div className="mr-10">
-          <img src="nithindi.png" className='ml-5 sm:ml-0 bg-black h-10' alt="Nit-hindi" />
-            <img src="CSE_Department.jpg" className='ml-5 sm:ml-0' alt="" />
-           <div className='grid sm:grid-cols-3 gap-12 mt-5 ml-12 sm:ml-0'>
-             {users.map((user , i)=>{
-               return <ProfileCard key={i} user={user}/>
-             })}
-           </div>
+          <div className="mr-5">
+            <img src="nithindi.png" className='ml-1.5 sm:ml-0 bg-black h-10' alt="Nit-hindi" />
+            <img src="CSE_Department.jpg" className=' ml-1.5 sm:ml-0' alt="" />
+            <div className='grid sm:grid-cols-3 gap-12 mt-5 ml-2 sm:ml-0'>
+              {users.map((user, i) => {
+                return <ProfileCard key={i} user={user} />
+              })}
+            </div>
           </div>
           <div className="hidden md:inline">
             <PeersNews />
