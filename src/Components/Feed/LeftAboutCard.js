3855cefe3881cc10a7fc68a6b6edbe4bb877f990
Rajan kumar <krcpr007@gmail.com>
2022-04-09 16:43:26 +0530

    local storage auth added

--- src/Components/Feed/LeftAboutCard.js
+++ src/Components/Feed/LeftAboutCard.js
@@ -1,9 +1,10 @@
 import React, { useContext , useEffect } from 'react'
 import avatar from '../assets/img_avatar.png'
 import { Link } from 'react-router-dom';
-import {auth} from '../../Firebase'
+// import {auth} from '../../Firebase'
 import  ContextProvider  from '../context/ContextProvider';
 function LeftAboutCard() {
+    const localAuth = JSON.parse(localStorage.getItem('st-hub'));
     const {darkMode ,profileData,userInformation} = useContext(ContextProvider); 
     useEffect(() => {
         userInformation();
@@ -12,10 +13,10 @@ function LeftAboutCard() {
     
     return (
         <div className={`shadow mt-2 rounded ml-5 ${darkMode?'bg-slate-900 text-white':null}`}>
-            <div className='ml-0 mt-2 p-2'>
-               <Link to="/profile"><img src={profileData.profileImg?profileData.profileImg:null || avatar} alt="profile-pic" className='content-center rounded-full border-2 border-yellow-400 w-96' /></Link>
+            <div className='ml-0 mt-2 p-2 w-64'>
+               <Link to="/profile"><img src={profileData.profileImg?profileData.profileImg:null || avatar} alt="profile-pic" className='content-center rounded-full border-2 border-yellow-400' /></Link>
             </div>
-            <h1 className='mt-1 font-medium text-xl text-center px-5'>{auth.currentUser.displayName?`${auth.currentUser.displayName}`:`${profileData.name}`}</h1>
+            <h1 className='mt-1 font-medium text-xl text-center px-5'>{localAuth.displayName?`${localAuth.displayName}`:`${profileData.name}`}</h1>
             <p className='text-sm text-gray-500 px-5 text-center'>
             {profileData && profileData.headline}
             </p>
