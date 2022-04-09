76e4725d74fa239a20ec343e86d8c60ebb5ac15c
Rajan kumar <krcpr007@gmail.com>
2022-04-09 16:43:57 +0530

    local storage auth added

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -1,6 +1,6 @@
 import {useEffect, useState } from 'react'
 import SenderProfile from './SenderProfile'
-import { db , auth} from "../../Firebase";
+import { db } from "../../Firebase";
 import Loader from '../Loader/Loader'
 import {
   collection,
@@ -10,13 +10,14 @@ import {
 } from "firebase/firestore";
 
 function Messages() {
+  const localAuth = JSON.parse(localStorage.getItem('st-hub')); 
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
     useEffect(() => {
       setLoading(true)
       const usersRef = collection(db, "users");
       // create query object
-      const q = query(usersRef, where("uid", "not-in", [auth.currentUser.uid]));
+      const q = query(usersRef, where("uid", "not-in", [localAuth.uid]));
       // execute query
       const unsub = onSnapshot(q, (querySnapshot) => {
         let users = [];
@@ -29,6 +30,7 @@ function Messages() {
       });
 
       return () => unsub();
+       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
   if(loading){
     return <Loader/>    
@@ -37,7 +39,7 @@ function Messages() {
     <div className='md:flex'>
       <div className='lg:w-1/4'>
        {users.map((sender)=>{
-         return <SenderProfile key={sender.uid} sender={sender} user1={auth.currentUser.uid} />
+         return <SenderProfile key={sender.uid} sender={sender} user1={localAuth.uid} />
         })}
         </div>
         <div className="bg-slate-500 w-full">
