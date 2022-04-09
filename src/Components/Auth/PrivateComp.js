f2cde175804141be1f5934072f6f71ae1a9bc72d
Rajan kumar <krcpr007@gmail.com>
2022-04-09 16:41:43 +0530

    local Auth added to private comp

--- src/Components/Auth/PrivateComp.js
+++ src/Components/Auth/PrivateComp.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import {Outlet , Navigate} from 'react-router-dom'; 
-// import {getAuth} from 'firebase/auth'
-import {auth} from '../../Firebase'
 function PrivateComp() {
-
-    return auth.currentUser? <Outlet/>:<Navigate to="/login"/> 
+    const localAuth = JSON.parse(localStorage.getItem('st-hub'));
+    return localAuth? <Outlet/>:<Navigate to="/login"/> 
 }
 
 export default PrivateComp
