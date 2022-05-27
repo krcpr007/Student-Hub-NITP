b2fbd8aa3efe74fc1285e8310cacdab39a9616da
Rajan kumar <krcpr007@gmail.com>
2022-05-27 23:25:59 +0530

    code refcator and added autometically reloading after new post and url of img also fixed now

--- src/Components/Feed/NewPost.js
+++ src/Components/Feed/NewPost.js
@@ -1,17 +1,17 @@
 import avatar from '../assets/img_avatar.png';
 import { Link } from 'react-router-dom';
-import {db, storage} from '../../Firebase';
-import {MdDownloading} from 'react-icons/md'
+import { db, storage } from '../../Firebase';
 import React, { useContext, useEffect, useState } from 'react'
 import { IoMdPhotos } from 'react-icons/io';
 import { ref, getDownloadURL, uploadBytes, deleteObject } from 'firebase/storage';
-import { setDoc, doc, getDoc, serverTimestamp, Timestamp , addDoc , collection, updateDoc} from "firebase/firestore";
+import { setDoc, doc, getDoc, serverTimestamp, Timestamp, addDoc, collection, updateDoc } from "firebase/firestore";
 import { MdVideoLibrary, MdEventAvailable } from 'react-icons/md';
 import { AiFillCloseCircle } from 'react-icons/ai'
 import ContextProvider from '../context/ContextProvider';
-function NewPost() {
+import Loader from '../Loader/Loader';
+function NewPost({setGetNewPosts}) {
   const localAuth = JSON.parse(localStorage.getItem('st-hub'));
-  const uid = localAuth.uid; 
+  const uid = localAuth.uid;
   const { darkMode, userInformation, profileData } = useContext(ContextProvider);
   const [showModal, setShowModal] = useState(false);
   const [loader, setLoader] = useState(false);
@@ -26,54 +26,36 @@ function NewPost() {
     userInformation();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-  const CreatePost =async (e) => {
-    if (file) {
-      const uploadImg = async () => {
-        setLoader(true)
-        const imgRef = ref(storage, `posts/${new Date().getTime()} - ${file.name}`)
-        try {
-          
-        const snap = await uploadBytes(imgRef, file);
-        
-        const url = await getDownloadURL(ref(storage, snap.ref.fullPath))
-        
-        await  addDoc(collection(db, 'posts'),{
-          imgPath:url,
-          forDeletePath: snap.ref.fullPath,
-        })
-        setFile('');
-      } catch (e) {
-        console.log(e.message); 
-      }
-      setLoader(false); 
-      }
-      uploadImg();
-    }
-    e.preventDefault(); 
-    if(text.length!==0 || file){
+  const CreatePost = async (e) => {
+    e.preventDefault();
+    if (text.length !== 0 || file) {
       const imgRef = ref(storage, `posts/${new Date().getTime()} - ${file.name}`)
-        try {
-          
+      try {
+        setLoader(true); 
         const snap = await uploadBytes(imgRef, file);
-        
+        // url of the picture after uploading
         const url = await getDownloadURL(ref(storage, snap.ref.fullPath))
-        
-        setFile('');
-      await addDoc(collection(db, 'posts'), {
-        comments:['', ''],
-        text:text, 
-        uid:uid, 
-        postedAt:date,
-        likes:1,
-        imgPath:url,
-        forDeletePath: snap.ref.fullPath,
 
-      }); 
-      showAndHideModal();
-    }catch(e){
-      console.error('enter data first')
+        setFile('');
+        await addDoc(collection(db, 'posts'), {
+          comments: ['', ''],
+          text: text,
+          uid: uid,
+          postedAt: date,
+          likes: 0,
+          imgPath: file?url:null,
+          forDeletePath: file?snap.ref.fullPath:null,
+        });
+        setLoader(false);
+        showAndHideModal();
+        setGetNewPosts(true); 
+        // setGetNewPosts(false);
+      } catch (e) {
+        console.log(e)
+      }
+    } else {
+      console.error('Enter data first')
     }
-  }
   }
   return (
     <div className={`lg:w-3/4 sm:w-3/4 shadow rounded ${darkMode ? 'bg-slate-900 text-white' : null}`}>
@@ -116,36 +98,36 @@ function NewPost() {
               </button>
             </div>
             {/*body*/}
-              <form action="">
-            <div className="relative p-4 flex-auto">
+            <form action="">
+              <div className="relative p-4 flex-auto">
                 <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
                 <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type..." value={text} onChange={e => setText(e.target.value)} />
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" htmlFor="user_avatar">Upload file</label>
                 <input className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none focus:border-transparent dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file" accept="image/*" onChange={e => setFile(e.target.files[0])} />
-            </div>
-            {/*footer*/}
-            <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-              <button
-                className=" bg-slate-800 text-white active:bg-slate-600 font-bold uppercase text-sm p-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => setShowModal(false)}
-              >
-                <AiFillCloseCircle className="" />
-              </button>
-              <button
-                className="bg-rose-800 text-white active:bg-slate-600 font-bold uppercase text-sm px-3 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="submit" onClick={CreatePost}
-              >
-                Post
-              </button>
-              <button
-                className="bg-slate-800 text-white active:bg-slate-600 font-bold uppercase text-sm px-3 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-              >
-                {loader?(<MdDownloading className="text-xl text-yellow-400"/>):null}
-              </button>
-            </div>
-              </form>
+              </div>
+              {/*footer*/}
+              <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                <button
+                  className=" bg-slate-800 text-white active:bg-slate-600 font-bold uppercase text-sm p-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={() => setShowModal(false)}
+                >
+                  <AiFillCloseCircle className="" />
+                </button>
+                <button
+                  className="bg-rose-800 text-white active:bg-slate-600 font-bold uppercase text-sm px-3 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="submit" onClick={CreatePost}
+                >
+                  Post
+                </button>
+                <button title='Uploading status'
+                  className="bg-slate-800 text-white active:bg-slate-600 font-bold uppercase text-sm pl-2 py-1 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                >
+                  {loader ? (<Loader />) : null}
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>) : null}
