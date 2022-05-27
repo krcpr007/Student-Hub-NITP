903b5fbab4564c441d45f501c501c8ded68d0b80
Rajan kumar <krcpr007@gmail.com>
2022-05-27 23:02:53 +0530

    conversationText bg changed blue to rose

--- src/Components/Messages/ConversationsText.js
+++ src/Components/Messages/ConversationsText.js
@@ -17,7 +17,7 @@ function ConversationsText({ msg }) {
         <div className={`p-0 h-fit ${darkMode ? 'bg-gradient-to-r from-slate-900 to-slate-700 text-white' : "bg-white"}`} ref={scrollRef}>
             <div className="">
                 <div className={`${msg.to===auth.currentUser.uid?'flex':'flex flex-row-reverse'}`}>
-                    <p className={`my-1 px-4 py-0.5 ${msg.to===auth.currentUser.uid?'text-left text-black bg-yellow-500 w-auto rounded-r px-2 rounded-bl-3xl':'bg-blue-400 rounded-l-lg rounded-br-3xl'}`}>{decryptedText}
+                    <p className={`my-1 px-4 py-0.5 ${msg.to===auth.currentUser.uid?'text-left text-black bg-rose-400 w-auto rounded-r px-2 rounded-bl-3xl':'bg-blue-400 rounded-l-lg rounded-br-3xl'}`}>{decryptedText}
                       {msg.media ?(<img src={msg.media} alt='' />):null}
                         <br />
                         <small className='text-xs opacity-50'>
