6bca18ebfce1c75040ba21a99e843ab871fa1d83
Rajan kumar <krcpr007@gmail.com>
2022-09-14 23:57:30 +0530

    about modified image added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,12 +5,16 @@ function About() {
     <div>
       <div>
         <div>
-          <img src="nithindi.png" className='bg-slate-900 h-10' alt="Nit-hindi" />
+          <img src="homebg-1.jpg" className='bg-slate-900 w-full' alt="Nit-hindi" />
         </div>
         <div className='mt-5 px-2 sm:px-5'>
-          <h1 className='text-2xl'>Vision</h1>
+          <h1 className='text-3xl text-center'>Vision</h1>
           <hr />
-          <p className='opacity-75'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique id debitis unde, quaerat quod reprehenderit obcaecati veniam, temporibus nobis ut labore saepe earum pariatur exercitationem nostrum. Ipsa aliquam libero at!</p>
+          <p className='opacity-75 text-justify md:px-10'>Student hub for whole Nit Patna.Where students of Nit Patna can post their achievements, status, Projects, coding skills, and many more things. Here students can connect to each other and they can chat with any student of nit Patna. They can also check academic details and classes and many more things.</p>
+            
+          <h1 className='text-3xl text-center mt-10'>Mission</h1>
+          <hr />
+          <p className='opacity-75 text-justify md:px-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat aperiam delectus incidunt cupiditate eum quae fugiat, labore debitis officiis qui autem in, nesciunt dolores rerum velit voluptatem ducimus numquam iure.</p>
         </div>
       </div>
     </div>
