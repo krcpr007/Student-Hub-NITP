230defb23b8aa2ae242443adf1cd84f78491b4f5
Rajan kumar <krcpr007@gmail.com>
2022-11-25 17:25:02 +0530

    warning removed

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -54,8 +54,8 @@ function Messages() {
 
               <svg aria-label="Direct" className="_ab6-" color="#fff" fill="#fff" height="96" role="img" viewBox="0 0 96 96" width="96">
                 <circle cx="48" cy="48" fill="none" r="47" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className={` ${darkMode ? " stroke-white" : "stroke-black"}`}></circle>
-                <line fill="none" stroke="currentColor" strokeLinejoin="round" stroke-width="2" x1="69.286" x2="41.447" y1="33.21" y2="48.804" className={`stroke-width-2 ${darkMode ? " stroke-white" : "stroke-black"}`}></line>
-                <polygon fill="none" points="47.254 73.123 71.376 31.998 24.546 32.002 41.448 48.805 47.254 73.123" stroke="currentColor" stroke-linejoin="round" stroke-width="2" className={` ${darkMode ? " stroke-white" : "stroke-black"}`}></polygon>
+                <line fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth="2" x1="69.286" x2="41.447" y1="33.21" y2="48.804" className={`strokeWidth-2 ${darkMode ? " stroke-white" : "stroke-black"}`}></line>
+                <polygon fill="none" points="47.254 73.123 71.376 31.998 24.546 32.002 41.448 48.805 47.254 73.123" stroke="currentColor" strokeLinejoin="round" strokeWidth="2" className={` ${darkMode ? " stroke-white" : "stroke-black"}`}></polygon>
               </svg>
             </div>
             <div className='text-center'>
