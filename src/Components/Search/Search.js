867964d4dc84ca7e7b3fae09eeddbdf033cae858
Rajan kumar <krcpr007@gmail.com>
2022-11-26 15:01:34 +0530

    search improved

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -47,15 +47,14 @@ function Search() {
     <div>
       <div className={`${darkMode ? 'bg-slate-900 text-white' : null}`}>
         <div className="flex">
-          <div>
+          <div className='w-1/3'>
             <div className="hidden md:inline">
               <LeftAboutCard />
               <GroupsClub />
             </div>
           </div>
-          <div className="mx-5">
-            <img src="nithindi.png" className='ml-1.5 sm:ml-0 bg-black h-10' alt="Nit-hindi" />
-            <img src="CSE_Department.jpg" className=' ml-1.5 sm:ml-0' alt="" />
+          <div className="mx-5 w-full">
+            <img src="homebg-2.jpg" className=' ml-1.5 sm:ml-0' alt="" />
             <div className='grid sm:grid-cols-3 gap-12 mt-5 ml-2 sm:ml-0'>
               {users.map((user, i) => {
                 return loader?<Loader/>: <ProfileCard key={i} user={user} />
