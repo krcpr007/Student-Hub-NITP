61dd478c8ddff32c1c9f1e9f35d00d66e2a910a1
Rajan kumar <krcpr007@gmail.com>
2022-12-10 14:41:54 +0530

    remove request

--- src/Components/profile/Connection.js
+++ src/Components/profile/Connection.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import ContextProvider from '../context/ContextProvider';
 
 function Connection({ user }) {
-    const { sendConnectionRequest, profileData, undoConnectionRequest, userInformation, removeConnection } = useContext(ContextProvider);
+    const { sendConnectionRequest, profileData, undoConnectionRequest, userInformation, removeConnection, removeConnectionRequest } = useContext(ContextProvider);
     useEffect(() => {
         userInformation()
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -20,6 +20,7 @@ function Connection({ user }) {
             {(user?.connections?.includes(profileData?.uid) && profileData?.connections?.includes(user?.uid)) ? <>
                 <button className="text-md shadow-2xl mx-2 mt-2 text-yellow-400 outline-2 outline-yellow-400 hover:bg-yellow-400 border border-yellow-300 hover:text-white px-3 py-1 rounded-md" onClick={e => removeConnection(user)}>Remove</button>
             </> : null}
+            {profileData?.connectionRequests?.includes(user?.uid) ? <><button className=' shadow-2xl mx-2 mt-2 text-yellow-400 outline-2 outline-yellow-400 hover:bg-yellow-400 border border-yellow-300 hover:text-white px-3 py-1 rounded-md' onClick={() => removeConnectionRequest(user)}> Remove Request</button> </> : null}
         </>
     )
 }
