41169659debe0f3ef5e11dd67e72dfcbbdf93c8b
Rajan kumar <krcpr007@gmail.com>
2022-12-12 00:21:04 +0530

    darkMode removed

--- src/pages/Error/404.js
+++ src/pages/Error/404.js
@@ -1,22 +1,20 @@
-import React, {useContext} from 'react'
+import React from 'react'
 import bgimg from './login-hero.svg'
-import ContextProvider from '../../Components/context/ContextProvider'
 function Error() {
-  const {darkMode} = useContext(ContextProvider);
   return (
     <>
-    <div className={`p-0 h-fit ${darkMode ? 'bg-gradient-to-b from-slate-900 to-slate-700 text-white' : "bg-white"}`}>
-      <div className="md:grid h-screen grid-cols-2 gap-4 px-11 place-item-center">
-       <img src={bgimg} className="mt-0 py-2 md:w-3/4 w-full" alt="" />
-      <div className='font-mono md:mx-12 md:flex items-center'>
-        <div className='md:mx-8'>
-        <h1 className=' block md:text-9xl text-7xl text-center min-w-full font-extra-bold'>404</h1>
-        <span className='text-center block w-full font-bold text-2xl'>THE PAGE YOU ARE LOOKING FOR</span>
-        <span className='text-center block w-full font-bold text-2xl sm:mb-52'>DOES NOT EXIST</span>
+      <div className={`p-0 h-fit bg-white dark:bg-gradient-to-b dark:from-slate-900 dark:to-slate-700 dark:text-white`}>
+        <div className="md:grid h-screen grid-cols-2 gap-4 px-11 place-item-center">
+          <img src={bgimg} className="mt-0 py-2 md:w-3/4 w-full" alt="" />
+          <div className='font-mono md:mx-12 md:flex items-center'>
+            <div className='md:mx-8'>
+              <h1 className=' block md:text-9xl text-7xl text-center min-w-full font-extra-bold'>404</h1>
+              <span className='text-center block w-full font-bold text-2xl'>THE PAGE YOU ARE LOOKING FOR</span>
+              <span className='text-center block w-full font-bold text-2xl sm:mb-52'>DOES NOT EXIST</span>
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-    </div>
     </>
   )
 }
