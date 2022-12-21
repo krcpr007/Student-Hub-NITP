f9a91bd08547e1032a16bd5fca6359236fd3fd4d
Rajan kumar <krcpr007@gmail.com>
2022-12-21 10:30:26 +0530

    linkIfy to detect link

--- src/Components/Messages/ConversationsText.js
+++ src/Components/Messages/ConversationsText.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect} from 'react'
+import React, { useRef, useEffect } from 'react'
 import { auth } from '../../Firebase';
 import Moment from 'react-moment';
 const CryptoJS = require("crypto-js");
@@ -11,17 +11,23 @@ function ConversationsText({ msg }) {
     useEffect(() => {
         scrollRef.current?.scrollIntoView({ behavior: "smooth" });
     }, [msg])
+    // function to detect is any hyperlink is there in in text message
+    function linkIfy(text) {
+        let urlRegex = /(https?:\/\/[^\s]+)/g
+        return text.replace(urlRegex, function (url) {
+            return '<a style="color:#141B2A;text-decoration:underline" title="Link: Click to visit" target="_blank" href="' + url + '">' + url + '</a>';
+        });
+    }
     return (
         <div className="p-0 h-fit bg-gradient-to-r from-slate-500 to-yellow-100 dark:text-white bg-white" ref={scrollRef}>
             <div className="">
                 <div className={`${msg.to === auth?.currentUser?.uid ? 'flex' : 'flex flex-row-reverse'}`}>
                     <div className={`my-1 px-1 py-0.5 ${msg.to === auth?.currentUser?.uid ? 'text-left text-black bg-rose-400 w-auto rounded-r px-2 rounded-bl-3xl' : 'bg-blue-400 rounded-l-lg rounded-br-3xl'}`}>
-                        <p>{decryptedText}</p>
+                        <p className='font-medium font-serif' dangerouslySetInnerHTML={{ __html: linkIfy(decryptedText) }} />
                         {msg.media ? (<img src={msg.media} alt='my-pic' className='' />) : null}
                         <br />
                         <small className='text-xs opacity-50'>
-                            {<Moment fromNow>
-                                {msg.createdAt.toDate()}</Moment>}
+                            {<Moment fromNow>{msg.createdAt.toDate()}</Moment>}
                         </small>
                     </div>
                 </div>
